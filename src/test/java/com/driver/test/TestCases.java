a787a925475ada823925d9496bf24d90f9ef0e21
Prem Lijo <72649555+premlijo@users.noreply.github.com>
2025-01-15 10:01:59 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,13 @@
 package com.driver.test;
 
+import com.driver.A;
+import com.driver.B;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+    public void testMethodOverriding() {
+        B obj = new B();
+        assertEquals("Method is overridden in Extended class B", obj.meth());
+    }
 }

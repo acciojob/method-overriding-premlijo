3dc304c380ce3e4c0c68f77eac83b61c1646c161
Prem Lijo <72649555+premlijo@users.noreply.github.com>
2025-01-15 09:33:47 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth());
-
+        System.out.print(obj.meth());
     }
 }
 
